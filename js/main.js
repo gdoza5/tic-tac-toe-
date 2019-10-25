8445f3092b49afdec0511b8f495933ebcd6cb108
gdoza5 <gdoza5@gmail.com>
2019-10-25 15:05:36 -0500

    can add x to space

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
-console.log('sound check')
+/*----- constants -----*/
+const playerOne = "x"
+const playerTwo = "o"
+
+
+
+
+/*----- app's state (variables) -----*/
+
+let playerOneMvs, playerTwoMvs, allMvs
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+
+
+/*----- event listeners -----*/
+
+document.getElementById('board').addEventListener('click', handleBoxClick);
+
+
+
+
+
+
+
+
+
+
+
+/*----- functions -----*/
+
+ 
+function handleBoxClick(evt) {
+    evt.target.textContent = playerOne
+    console.log('box was clicked')
+}
+
+function init() {
+    allMvs =[]
+};
